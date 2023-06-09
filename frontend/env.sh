d262ce250cc6cf2cf03a9ce384fb111fea3744f0
rizalord <ahmadkhamdani9@gmail.com>
2023-06-10 02:12:02 +0700

    Update

--- frontend/env.sh
+++ frontend/env.sh
@@ -12,15 +12,10 @@ echo "window._env_ = {" >> ./env-config.js
 while read -r line || [[ -n "$line" ]];
 do
     # Split env variables by character `=`
-    if printf '%s\n' "$line" | grep -q -e '='; then
-        varname=$(printf '%s\n' "$line" | sed -e 's/=.*//')
-        varvalue=$(printf '%s\n' "$line" | sed -e 's/^[^=]*=//')
-    fi
-
-    # Read value of current variable if exists as Environment variable
-    value=$(printf '%s\n' "${!varname}")
-    # Otherwise use value from .env file
-    [[ -z $value ]] && value=${varvalue}
+    varname=$(printf '%s\n' "$line" | sed -e 's/=.*//')
+    varvalue=$(printf '%s\n' "$line" | sed -e 's/^[^=]*=//')
+    
+    value=${varvalue}
     
     # Append configuration property to JS file
     echo "  $varname: \"$value\"," >> ./env-config.js
