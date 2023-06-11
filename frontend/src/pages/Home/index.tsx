b94a441ebf73325f10e383bafb1abed5e044e6b0
rizalord666 <whoaminsis0@gmail.com>
2023-06-11 18:18:02 +0700

    test membuat banner di halaman home

--- frontend/src/pages/Home/index.tsx
+++ frontend/src/pages/Home/index.tsx
@@ -1,4 +1,19 @@
 export default function Home() {
-  console.log(window._env_.API_URL)
-  return <p>Home</p>
+  return <div className="bg-cover bg-no-repeat bg-center py-36 relative">
+    <div className="container">
+      <h1 className="xl:text-6xl md:text-5xl text-4xl text-gray-800 font-medium mb-4">
+        Best Collection For <br className="hidden sm:block" /> Home Decoration
+      </h1>
+      <p className="text-base text-gray-600 leading-6">
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Culpa <br className="hidden sm:block" />
+        assumenda aliquid inventore nihil laboriosam odio
+      </p>
+      <div className="mt-12">
+        <a href="shop.html" className="bg-primary border border-primary text-white px-8 py-3 font-medium rounded-md uppercase hover:bg-transparent
+               hover:text-primary transition">
+          Shop now
+        </a>
+      </div>
+    </div>
+  </div>
 }
