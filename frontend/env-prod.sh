a55583ad64929d9660d56a98218163f38fa22abe
rizalord <ahmadkhamdani9@gmail.com>
2023-07-03 11:44:36 +0700

    fix commit

--- frontend/env-prod.sh
+++ frontend/env-prod.sh
@@ -1,24 +1,3 @@
-#!/bin/bash
-
-# Recreate config file
-rm -rf ./dist/env-config.js
-touch ./dist/env-config.js
-
-# Add assignment 
-echo "window._env_ = {" >> ./dist/env-config.js
-
-# Read system environment variable
-echo "  API_URL: \"$API_URL\"," >> ./dist/env-config.js
-echo "  ALGOLIA_APP_ID: \"$ALGOLIA_APP_ID\"," >> ./dist/env-config.js
-echo "  ALGOLIA_SEARCH_KEY: \"$ALGOLIA_SEARCH_KEY\"," >> ./dist/env-config.js
-echo "  ALGOLIA_INDEX: \"$ALGOLIA_INDEX\"," >> ./dist/env-config.js
-
-echo "}" >> ./dist/env-config.js
-
-# Copy config file to /dist folder
-cp ./env-config.js ./dist/env-config.js
-cp ./env-config.js ./public/env-config.js
-
 # Create Env Prod
 rm -rf ./.env.prod
 touch ./.env.prod
