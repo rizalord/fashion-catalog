c3c18447d90524311d37c9f403bf01b8826e2a98
rizalord <ahmadkhamdani9@gmail.com>
2023-07-03 11:13:16 +0700

    fix ci cd

--- frontend/env-prod.sh
+++ frontend/env-prod.sh
@@ -1,19 +1,11 @@
 #!/bin/bash
 
 # Recreate config file
-rm -rf ./env-config.js
-touch ./env-config.js
+rm -rf ./.env.prod
+touch ./.env.prod
 
-# Add assignment 
-echo "window._env_ = {" >> ./env-config.js
-
-# Read system environment variable: "API_URL"
-echo "  API_URL: \"$API_URL\"," >> ./env-config.js
-echo "  ALGOLIA_APP_ID: \"$ALGOLIA_APP_ID\"," >> ./env-config.js
-echo "  ALGOLIA_SEARCH_KEY: \"$ALGOLIA_SEARCH_KEY\"," >> ./env-config.js
-echo "  ALGOLIA_INDEX: \"$ALGOLIA_INDEX\"," >> ./env-config.js
-
-echo "}" >> ./env-config.js
-
-# Copy config file to /dist folder
-cp ./env-config.js ./dist/env-config.js
+# Read system environment variable
+echo "API_URL=$API_URL" >> ./.env.prod
+echo "ALGOLIA_APP_ID=$ALGOLIA_APP_ID" >> ./.env.prod
+echo "ALGOLIA_SEARCH_KEY=$ALGOLIA_SEARCH_KEY" >> ./.env.prod
+echo "ALGOLIA_INDEX=$ALGOLIA_INDEX" >> ./.env.prod
